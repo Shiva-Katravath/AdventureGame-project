3402910f4b653e5f6560153a74ac86788d9d3d3f
shiva <shiva@shivas-MacBook-Pro.local>
2025-08-11 20:06:53 +0530

    Added new lines

--- adventureGame.js
+++ adventureGame.js
@@ -1,12 +1,26 @@
+//========================================
+//The Dragon's Quest - Text Adventure Game
+//A Progression-based learning project
+//========================================
+
+//Including readline for player input
 const readline = require('readline-sync');
 
+//Game state variables
 let playerName = "";
 let playerHealth = 100;
 let playerGold = 20;
 let currentlocation = "village";
 let gameRunning = true;
 let inventory = [];
-// readline.question();
+
+
+console.log("=====================================");
+console.log("           The Dragon's Quest.       ");
+console.log("=====================================");
+console.log("\nYour quest: Defeat the dragon in the mountains!");
+
+//get player name
 playerName = readline.question("\nwhat is yourname,brave adventurer?");
-console.log("welcome to the game", playerName + "!");
+console.log("welcome to the game, " + playerName + "!");
 console.log("you start with" + playerGold + "gold.")
